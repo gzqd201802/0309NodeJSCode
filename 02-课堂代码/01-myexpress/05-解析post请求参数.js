12ae62176964cfbcc242280c2ee9d420bbc2df6f
sujiehao <sujiehao@itcast.cn>
2020-03-09 11:45:08 +0800

    资料提交

--- 02-课堂代码/01-myexpress/05-解析post请求参数.js
+++ 02-课堂代码/01-myexpress/05-解析post请求参数.js
@@ -9,7 +9,7 @@ console.log(bodyParser);
 const app = express();
 
 // 启动 body-parser， application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // 3. 后端路由
 // request    请求报文 - 客户端发过来的
 // response   响应报文 - 服务器返回去的
@@ -24,9 +24,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.post('/api/user/register', (request, response) => {
     // 默认现象：请求报文中找不到 post 传递过来的数据
     console.log('请求报文', request);
-    console.log(request.body);
-    
-    response.send('2.以post方式请求根路径，回来的信息2');
+    console.log('request.body', request.body);
+    // 有了 body-parser 后，可以中 request.body 获取 post 请求的参数
+    // 根据文档要求，解析出 username 参数
+    const { username } = request.body;
+    // 判断是否为空
+    if (username.trim() === '' || username === undefined) {
+        response.send('用户名不能为空');
+    }else{
+        // 如果不为空，就返回注册成功
+        response.send(`${username}注册成功`);
+    }
 });
 
 // 4. 监听端口
